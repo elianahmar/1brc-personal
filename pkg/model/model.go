f8fe5ab98fce43911be108badfb809b516c9d902
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2026-05-05 23:15:40 -0500

    moved structs to model

---
+++ pkg/model/model.go
@@ -0,0 +1,16 @@
+package model
+
+type Validation struct {
+	Min float64
+	Max float64
+	Avg float64
+}
+
+type Measurement struct {
+	City  string
+	Temps float64
+	Count int
+	Min   float64
+	Max   float64
+	Avg   float64
+}
