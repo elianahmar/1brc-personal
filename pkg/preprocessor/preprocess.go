52a260734a15b4a53b2da5b7a3b61f7524d094aa
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2026-05-05 23:30:27 -0500

    utils package; fixed preprocess

--- pkg/preprocessor/preprocess.go
+++ pkg/preprocessor/preprocess.go
@@ -2,24 +2,39 @@ package preprocessor
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/throwea/1brc-go/pkg/model"
+	"github.com/throwea/1brc-go/pkg/utils"
 )
 
-func collectData(data) {
-	measurements := make(map[model.City]model.Measurement)
+func collectData(data chan string) {
+	measurements := make(map[model.City]*model.Measurement)
 	for text := range data {
 		measurement := processLine(text)
 		split := strings.Split(text, ";")
 		city := model.City(split[0])
 		if _, exists := measurements[city]; !exists {
-			measurements[city] = model.Measurement{}
+			measurements[city] = &model.Measurement{}
 		}
-		measurements[city].Temp += measurement.temp
+		measurements[city].Temps += measurement.Temps
 		measurements[city].Count += 1
 		fmt.Printf("%v\n", measurement)
 	}
 }
 
 // }(data)
+
+func processLine(text string) model.Measurement {
+	split := strings.Split(text, ";")
+	dig, err := strconv.ParseFloat(split[1], 64)
+	if err != nil {
+		panic(err)
+	}
+	temp := utils.TruncateNaive(dig, 0.1) // No good. We don't need this much precision
+	return model.Measurement{
+		City:  split[0],
+		Temps: temp,
+	}
+}
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,6 +2,6 @@ package pkg
 
 import "math"
 
-func truncateNaive(f float64, unit float64) float64 {
+func TruncateNaive(f float64, unit float64) float64 {
 	return math.Trunc(f/unit) * unit
 }
