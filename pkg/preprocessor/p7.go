235e1b579fa93108adad208d41608d24e24c3b68
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2026-05-12 05:23:48 -0500

    inlined unsafe calls

--- pkg/preprocessor/p7.go
+++ pkg/preprocessor/p7.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"os"
 	"strconv"
+	"unsafe"
 
 	"github.com/throwea/1brc-go/pkg/model"
 	"github.com/throwea/1brc-go/pkg/utils"
@@ -34,8 +35,8 @@ func (p7 *P7) Compute() map[string]*model.Measurement { // 53 seconds. Minimal d
 		line := fileScanner.Bytes() // NOTE: unsafe is no good here. Per the docs. The underlying array can be overwritten
 		// process the line itself
 		city, num, _ := bytes.Cut(line, delim) // Returns original array. Unsafe is no good here either
-		temp, _ := strconv.ParseFloat(utils.BytesToString(num), 64)
-		measurement, exists := measurements[utils.BytesToString(city)] // Lookup trick. city underlying byte array can change but we can use it for lookup
+		temp, _ := strconv.ParseFloat(unsafe.String(&num[0], len(num)), 64)
+		measurement, exists := measurements[unsafe.String(&city[0], len(city))] // Lookup trick. city underlying byte array can change but we can use it for lookup
 		if !exists {
 			cityName := string(city)
 			measurement = &model.Measurement{City: cityName}
