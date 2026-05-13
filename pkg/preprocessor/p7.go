6b713c62171030c4b38b3b2beffda054991762a4
Elian Ahmar <54604093+throwea@users.noreply.github.com>
2026-05-13 00:39:35 -0500

    Inlining (#18)
    * inlined unsafe calls
    
    * code cleanup
    
    * updated performance of p7 implementation
    
    ---------
    
    Co-authored-by: Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>

--- pkg/preprocessor/p7.go
+++ pkg/preprocessor/p7.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"os"
 	"strconv"
+	"unsafe"
 
 	"github.com/throwea/1brc-go/pkg/model"
 	"github.com/throwea/1brc-go/pkg/utils"
@@ -15,14 +16,13 @@ type P7 struct {
 	ChanSize int
 }
 
-func NewP7(path string, chansize int) *P7 {
+func NewP7(path string) *P7 {
 	return &P7{
-		Path:     path,
-		ChanSize: chansize,
+		Path: path,
 	}
 }
 
-func (p7 *P7) Compute() map[string]*model.Measurement { // 53 seconds. Minimal difference using unsafe for temperature
+func (p7 *P7) Compute() map[string]*model.Measurement { // 51 seconds. Minimal difference using unsafe for temperature
 	// Brute force this. Read line by line and update a table
 	file := utils.PanicE(os.Open(p7.Path))
 	defer file.Close()
@@ -34,8 +34,8 @@ func (p7 *P7) Compute() map[string]*model.Measurement { // 53 seconds. Minimal d
 		line := fileScanner.Bytes() // NOTE: unsafe is no good here. Per the docs. The underlying array can be overwritten
 		// process the line itself
 		city, num, _ := bytes.Cut(line, delim) // Returns original array. Unsafe is no good here either
-		temp, _ := strconv.ParseFloat(utils.BytesToString(num), 64)
-		measurement, exists := measurements[utils.BytesToString(city)] // Lookup trick. city underlying byte array can change but we can use it for lookup
+		temp, _ := strconv.ParseFloat(unsafe.String(&num[0], len(num)), 64)
+		measurement, exists := measurements[unsafe.String(&city[0], len(city))] // Lookup trick. city underlying byte array can change but we can use it for lookup
 		if !exists {
 			cityName := string(city)
 			measurement = &model.Measurement{City: cityName}
