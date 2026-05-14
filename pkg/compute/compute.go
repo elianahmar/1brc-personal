73849992af2b408192523c48fa443c2672aee99d
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2026-05-13 22:19:02 -0500

    convertToStr method

--- pkg/compute/compute.go
+++ pkg/compute/compute.go
@@ -2,7 +2,8 @@ package compute
 
 import (
 	"fmt"
-	"math"
+	"strconv"
+	"strings"
 
 	"github.com/throwea/1brc-go/pkg/model"
 	"github.com/throwea/1brc-go/pkg/utils"
@@ -17,16 +18,48 @@ func ComputeAvg(measurements map[string]*model.Measurement) {
 }
 
 // NOTE: I can just convert to string then simply put a period before smallest digit. Done. Try that if computations don't work
-func ComputeAvgInt(measurements map[string]*model.MeasurementInt) map[string]*model.Predicted {
+//
+//	func ComputeAvgInt(measurements map[string]*model.MeasurementInt) map[string]*model.Predicted {
+//		predictions := make(map[string]*model.Predicted)
+//		for city, measurement := range measurements {
+//			avg := math.Round(float64(measurement.Temps) / float64(measurement.Count))
+//			avg /= 10
+//			predictions[city] = &model.Predicted{
+//				Min: utils.TruncateNaive(float64(measurement.Min)/10.0, 0.1),
+//				Max: utils.TruncateNaive(float64(measurement.Max)/10.0, 0.1),
+//				Avg: utils.TruncateNaive(math.Round(avg), 0.1),
+//			}
+//		}
+//		return predictions
+//	}
+
+func ComputeAvgStrConv(measurements map[string]*model.MeasurementInt) map[string]*model.Predicted {
 	predictions := make(map[string]*model.Predicted)
 	for city, measurement := range measurements {
-		avg := math.Round(float64(measurement.Temps) / float64(measurement.Count))
-		avg /= 10
+		min, max, avg := measurement.Min/10, measurement.Max/10, (measurement.Temps/measurement.Count)/10
 		predictions[city] = &model.Predicted{
-			Min: utils.TruncateNaive(float64(measurement.Min)/10.0, 0.1),
-			Max: utils.TruncateNaive(float64(measurement.Max)/10.0, 0.1),
-			Avg: utils.TruncateNaive(math.Round(avg), 0.1),
+			City: city,
+			Min:  convertToStr(min),
+			Max:  convertToStr(max),
+			Avg:  convertToStr(avg),
 		}
 	}
 	return predictions
+}
+
+func convertToStr(num int) string {
+	res := strings.Builder{}
+	numStr := strconv.Itoa(num)
+	N := len(numStr)
+	utils.PanicIf(N > 5, "") // Temp should never exceed 5 bytes (ex. -45.4)
+	for i := 0; i < N+1; i++ {
+		if i == N {
+			res.WriteByte('.')
+			continue
+		}
+		res.WriteByte(numStr[i])
+	}
+	finalStr := res.String()
+	fmt.Printf("before: %s, after: %s\n", numStr, finalStr)
+	return res.String()
 }
