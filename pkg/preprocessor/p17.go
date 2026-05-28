add5a9c38b35508144eb0bc00921f63d8fe9da1f
Elian Ahmar <elianahmar@Elians-MacBook-Pro.local>
2026-05-27 23:45:01 -0500

    move consts out; this is concurrent safe

--- pkg/preprocessor/p17.go
+++ pkg/preprocessor/p17.go
@@ -10,6 +10,14 @@ import (
 	"github.com/throwea/1brc-go/pkg/model"
 )
 
+const (
+	semicolon = byte(';')
+	newline   = byte('\n')
+	minus     = byte('-')
+	zero      = byte('0')
+	nine      = byte('9')
+)
+
 type P17 struct {
 	Path     string
 	ChanSize int
@@ -21,7 +29,7 @@ func NewP17(path string) *P17 {
 	}
 }
 
-func (p17 *P17) Compute() map[string]*model.MeasurementInt { // 4.5 seconds.
+func (p17 *P17) Compute() map[string]*model.MeasurementInt { // 3.8 seconds
 
 	rChan := make(chan model.Range, 1000) // TODO: make this configurable
 	rSig := make(chan bool)
@@ -39,36 +47,11 @@ func (p17 *P17) Compute() map[string]*model.MeasurementInt { // 4.5 seconds.
 				p17.processRange(r, mChan, file, wg)
 			}(r, mChan, file, wg)
 		}
-		// BUG: I see the issue I think. We receive all of the ranges and spawn go routines
-		// To process them, but we aren't actually done, until all of those go routines complete
-		// I could cheat since I know we have to process 3290. But I want to implement this with the assumption that
-		// I don't know how many ranges appear. That will allow me to tune different params like the chunk size
-		//
-		// BUG:Hack solution, ChunkFileAsync can push the total number of ranges to a chan
-		// In the main thread, I can count if we have received all ranges and push true to rSig
-		// Process Range knows how many times
-		//
-		// NOTE: fix was simple. just add to waitgroup on every receive from rChan...
 		wg.Wait()
 		rSig <- true
 	}(mChan, file)
-	// Spawn another go routine which waits for all ranges to be processed and closes
-	// the channel so localMeasurement := range mChan can exit after it drains the channel
+
 	go func(rSig chan bool) {
-		// TODO: how do I synchronize in this case?
-		// I'll have the first go routine producing the ranges and pushing to rChan
-		// The second go routine will be consuming from range chan
-		// Then lastly, on the main thread, I'm receiving from mChan
-		//
-		// 1. So I think the first thing I'll need to do is spawn a go routine for producing the
-		// the ranges. Inside that function we close the channel after we are done
-		// 2. Then the second go routine will be for processing the ranges. The second go routine
-		// is processing the range and pushing the map to the mChan
-		// 3. On the main thread I need to consume from the mChan until it's closed
-		//
-		//PERF: I can create a separate channel which will block until it receives a value... Easy
-		//
-		//rChan -> mChan -> main
 		<-rSig
 		close(mChan)
 	}(rSig)
@@ -77,7 +60,6 @@ func (p17 *P17) Compute() map[string]*model.MeasurementInt { // 4.5 seconds.
 	for localMeasurement := range mChan {
 		for city, newMeasure := range localMeasurement {
 			measurement, exists := finalMeasure[city]
-			// IDEA: could we avoid allocating a new object if !exists and just assign
 			if !exists {
 				measurement = newMeasure
 				finalMeasure[city] = measurement
@@ -104,19 +86,9 @@ func (p17 *P17) processRange(r model.Range, mChan chan map[string]*model.Measure
 	}
 	buff = buff[:n] // NOTE: need to do this because it isn't guaranteed that I'll read in all of the bytes
 
-	const (
-		semicolon = byte(';')
-		newline   = byte('\n')
-		period    = byte('.')
-		minus     = byte('-')
-		zero      = byte('0')
-		nine      = byte('9')
-	)
-
-	N := len(buff)
 	ptr := 0
 	temp := 0
-	for ptr < N {
+	for ptr < n {
 		// Read up to the ';'
 		start := ptr
 		for buff[ptr] != semicolon {
@@ -126,18 +98,18 @@ func (p17 *P17) processRange(r model.Range, mChan chan map[string]*model.Measure
 		city := unsafe.String(&buff[start], ptr-start)
 		// Move the ptr forward off the semicolon
 		ptr++
-		isNeg := buff[ptr] == minus
 		temp = 0
-		for ptr < N {
+		for ptr < n {
 			nb := buff[ptr]
+			if nb == newline {
+				break
+			}
 			if zero <= nb && nb <= nine {
 				temp = temp*10 + int(nb-zero)
-			} else if nb == newline {
-				break
 			}
 			ptr++
 		}
-		if isNeg {
+		if buff[cityEnd+1] == minus {
 			temp *= -1
 		}
 		ptr++ // move past newline
